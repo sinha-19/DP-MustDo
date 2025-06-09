3bf20d2c9ab7f921b67ce09a495c16698cb7d051
Saket Kumar Sinha <133163978+sinha-19@users.noreply.github.com>
2025-06-09 22:16:52 +0530

    Create TriangleMinPathSum.java

---
+++ Day 2/TriangleMinPathSum.java
@@ -0,0 +1,19 @@
+/*
+Problem: Minimum Path Sum in Triangular Grid (DP-11)
+Approach: Bottom-Up Tabulation from bottom of triangle to top.
+Time Complexity: O(n*n)
+*/
+public class TriangleMinPathSum {
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) dp[i] = triangle.get(n - 1).get(i);
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+}
