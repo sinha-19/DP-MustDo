d3b2531505196de5a339ce18ff22e4f98eb860c4
Saket Kumar Sinha <133163978+sinha-19@users.noreply.github.com>
2025-06-09 22:14:34 +0530

    Create GridUniquePaths.java

---
+++ Day 2/GridUniquePaths.java
@@ -0,0 +1,19 @@
+/*
+Problem: Grid Unique Paths (DP-8)
+Approach: Bottom-Up Tabulation using DP table to count paths from (0,0) to (m-1,n-1).
+Time Complexity: O(m*n)
+*/
+
+public class GridUniquePaths {
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int j = 0; j < n; j++) dp[0][j] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
